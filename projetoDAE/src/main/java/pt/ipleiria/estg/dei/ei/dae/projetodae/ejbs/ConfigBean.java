fd44b075a2986bd283c6cd95c66fe18cd2a9482b
Vascofer1 <122946611+Vascofer1@users.noreply.github.com>
2024-12-26 13:14:56 +0000

    Add files via upload

---
+++ projetoDAE/src/main/java/pt/ipleiria/estg/dei/ei/dae/projetodae/ejbs/ConfigBean.java
@@ -0,0 +1,15 @@
+package pt.ipleiria.estg.dei.ei.dae.projetodae.ejbs;
+import jakarta.annotation.PostConstruct;
+import jakarta.ejb.Singleton;
+import jakarta.ejb.Startup;
+
+
+    @Startup
+    @Singleton
+    public class ConfigBean {
+        @PostConstruct
+        public void populateDB() {
+            System.out.println("Hello Java EE!");
+        }
+    }
+
