9d1be5e638eb2501fe13389ef23087b95334c285
João Roxo <2221411@my.ipleiria.pt>
2025-01-18 14:09:47 +0000

    Inclusão de exceptions

---
+++ projetoDAE/src/main/java/pt/ipleiria/estg/dei/ei/dae/projetodae/exceptions/MyEntityNotFoundException.java
@@ -0,0 +1,11 @@
+package pt.ipleiria.estg.dei.ei.dae.projetodae.exceptions;
+
+public class MyEntityNotFoundException extends Exception {
+
+
+
+    public MyEntityNotFoundException(String message) {
+        super(message);
+    }
+
+}
